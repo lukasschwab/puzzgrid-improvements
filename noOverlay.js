0a37b04277e877c4ddf0ee8ad69c742bea3716db
Lukas Schwab <lukas@vanta.com>
2019-12-10 21:44:53 -0800

    Revert

--- noOverlay.js
+++ noOverlay.js
@@ -1,10 +1,6 @@
 // Remove overlays created by intentional intervention (e.g. finishing a puzzle,
 // or guess the last group).
 
-// "You solved the grid!"
-const gridcomplete = document.getElementById("gridcomplete");
-gridcomplete.parentNode.removeChild(gridcomplete)
-
 // "Rate this grid"
 const rating = document.getElementById("rating");
 rating.parentNode.removeChild(rating);
