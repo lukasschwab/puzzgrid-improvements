cbcf0202a8441e97cee66c4df0246b66792e4464
Lukas Schwab <lukas@vanta.com>
2019-12-10 21:31:33 -0800

    Remove grid complete overlay

--- noOverlay.js
+++ noOverlay.js
@@ -1,2 +1,10 @@
-const ratingOverlay = document.getElementById("rating");
-ratingOverlay.parentNode.removeChild(ratingOverlay);
+// Remove overlays created by intentional intervention (e.g. finishing a puzzle,
+// or guess the last group).
+
+// "You solved the grid!"
+const gridcomplete = document.getElementById("gridcomplete");
+gridcomplete.parentNode.removeChild(gridcomplete)
+
+// "Rate this grid"
+const rating = document.getElementById("rating");
+rating.parentNode.removeChild(rating);
